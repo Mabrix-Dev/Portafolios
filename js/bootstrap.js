0c7c5f52bec62c17a5b6854e2d256c0f2d3c5431
Mauricio <maurixio.contreras@gmail.com>
2022-11-14 14:40:11 -0300

    segundo commmit

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -16,19 +16,10 @@ if (typeof jQuery === 'undefined') {
   }
 }(jQuery);
 
-/* ========================================================================
- * Bootstrap: transition.js v3.3.4
- * http://getbootstrap.com/javascript/#transitions
- * ========================================================================
- * Copyright 2011-2015 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
-
 +function ($) {
   'use strict';
 
-  // CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)
+  // CSS TRANSITION SUPPORT 
   // ============================================================
 
   function transitionEnd() {
